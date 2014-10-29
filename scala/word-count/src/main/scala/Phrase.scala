d849321501e4535f837b22e8ae1290ae32d293d9
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-10-29 00:34:43 -0300

    extract count function to make it read better

--- scala/word-count/src/main/scala/Phrase.scala
+++ scala/word-count/src/main/scala/Phrase.scala
@@ -1,14 +1,17 @@
 class Phrase(content: String) {
-
-  lazy val words = content.toLowerCase.split(Phrase.OnlyWordsRegex)
-
-  lazy val wordCount = words.foldLeft(Phrase.EmptyMap) { case (map, word) =>
-    val count = map.get(word).getOrElse(0)
-    map + (word -> (count + 1))
-  }
+  import Phrase._
+  lazy val words = content.toLowerCase.split(OnlyWordsRegex)
+  lazy val wordCount = words.foldLeft(EmptyMap)(CountWords)
 }
 
 object Phrase {
+  type WordCount = Map[String, Int]
+
   private val OnlyWordsRegex = "[^a-z0-9']+"
-  private val EmptyMap = Map.empty[String, Int]
+  private val EmptyMap = Map.empty[String, Int].withDefaultValue(0)
+  private val CountWords: (WordCount, String) => WordCount = {
+    case (map, word) =>
+      val count = map(word)
+      map + (word -> (count + 1))
+  }
 }
