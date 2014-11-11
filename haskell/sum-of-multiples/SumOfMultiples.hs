5ca26ebb2dbca9ebb0896becb38d5c31bdb11567
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-10 23:15:48 -0300

    a bit of dotfree sintax and simplification

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -6,9 +6,9 @@ sumOfMultiplesDefault :: Int -> Int
 sumOfMultiplesDefault = sumOfMultiples [3, 5]
 
 sumOfMultiples :: [Int] -> Int -> Int
-sumOfMultiples ns n = sum multiples
+sumOfMultiples divisors n = sum multiples
   where
-    multiples = [x | x <- [1..n-1], isMultiple x]
-    isMultiple x = any (\f -> f x) multiples'
-    multiples' = map isMultipleOf ns
-    isMultipleOf y x = (x `mod` y == 0)
+    multiples = [x | x <- [1..n-1], anyMultiple x]
+    anyMultiple x = isMultipleOf `any` divisors
+      where
+        isMultipleOf = (==0) . (mod x)
