ce80aa225cf92d3c47c497297dd7c59de169fcbc
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-10 23:54:58 -0300

    anagrams in scala

---
+++ scala/anagram/src/main/scala/Anagram.scala
@@ -0,0 +1,31 @@
+class Anagram(text: String) {
+
+  val cannonicalText = cannonical(text)
+
+  def cannonical(string: String): String =
+    string.toLowerCase.sortWith(_ > _)
+
+  def matches(candidates: Seq[String]): Seq[String] = {
+    def isAnagram(candidate: String) =
+      cannonical(candidate) == cannonicalText
+
+    def sameWord(candidate: String) =
+      candidate.toLowerCase == text.toLowerCase
+
+    def mustBeIncluded(candidate: String, rest: Seq[String]) = {
+      val alreadyIncluded =
+        rest.map(_.toLowerCase).contains(candidate.toLowerCase)
+
+      isAnagram(candidate) && !sameWord(candidate) && !alreadyIncluded
+    }
+
+    def pickAnagrams(rest: Seq[String], candidate: String): Seq[String] = {
+      if (mustBeIncluded(candidate, rest))
+        rest ++ Seq(candidate)
+      else
+        rest
+    }
+
+    candidates.foldLeft(Seq.empty[String])(pickAnagrams)
+  }
+}
