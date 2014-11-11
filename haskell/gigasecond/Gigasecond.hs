605655828bbbf0051ec8d6d155b4127200497d26
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-10 22:22:39 -0300

    add Wall header and explicit type annotation

--- haskell/gigasecond/Gigasecond.hs
+++ haskell/gigasecond/Gigasecond.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Gigasecond (fromDay) where
 
 import Data.Time.Calendar (Day, addDays)
@@ -5,5 +7,5 @@ import Data.Time.Calendar (Day, addDays)
 fromDay :: Day -> Day
 fromDay day = addDays daysToGigasecond day
   where daysToGigasecond = gigasecond `div` secondsInDay
-        gigasecond = 10 ^ 9
+        gigasecond = 10 ^ (9 :: Int)
         secondsInDay = 24 * 60 * 60
