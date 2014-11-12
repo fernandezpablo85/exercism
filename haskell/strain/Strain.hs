74832b5a98a718bcfc3f64369a4fc09940494c12
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-12 10:18:12 -0300

    added recursion version as suggested, eta reduction

--- haskell/strain/Strain.hs
+++ haskell/strain/Strain.hs
@@ -5,5 +5,9 @@ module Strain (keep, discard) where
 keep :: (a -> Bool) -> [a] -> [a]
 keep p xs = [x | x <- xs, p x]
 
+keep' :: (a -> Bool) -> [a] -> [a]
+keep' _ [] = []
+keep' p (x : xs) = if p x then x : keep' p xs else keep' p xs
+
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = keep (not . p) xs
+discard p = keep (not . p)
