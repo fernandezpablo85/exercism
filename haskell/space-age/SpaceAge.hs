a831f4058bbaffc913032f6d6ea70aa539fc7b65
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-16 22:35:20 -0300

    suggestions by etrepum

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -5,14 +5,16 @@ module SpaceAge(Planet(..), ageOn) where
 data Planet = Earth | Venus | Mercury | Mars | Jupiter | Saturn | Uranus | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet days = days / (period planet)
+ageOn planet days = days / period planet
 
 period :: Planet -> Float
-period Earth = 31557600
-period Mercury = period Earth * 0.2408467
-period Venus = period Earth * 0.61519726
-period Mars = period Earth * 1.8808158
-period Jupiter = period Earth * 11.862615
-period Saturn = period Earth * 29.447498
-period Uranus = period Earth * 84.016846
-period Neptune = period Earth * 164.79132
+period planet = secondsPerYearInEarth * case planet of
+  Earth -> 1
+  Mercury -> 0.2408467
+  Venus -> 0.61519726
+  Mars -> 1.8808158
+  Jupiter -> 11.862615
+  Saturn -> 29.447498
+  Uranus -> 84.016846
+  Neptune -> 164.79132
+  where secondsPerYearInEarth = 31557600
