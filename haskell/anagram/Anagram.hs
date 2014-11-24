7174b2893fae7bce6b01ec20a6eb181d81c9ec4d
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-24 14:37:14 -0300

    force eager evaluation of norm a

--- haskell/anagram/Anagram.hs
+++ haskell/anagram/Anagram.hs
@@ -10,6 +10,6 @@ anagramsFor orig = filter (isAnagram orig)
 isAnagram :: String -> String -> Bool
 isAnagram a b = notSame && anagram
   where notSame = lower a /= lower b
-        anagram = norm a == norm b
+        anagram = let normA = norm a in seq normA (normA == norm b)
         norm = sort . lower
         lower = map toLower
