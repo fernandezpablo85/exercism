a5832eeb4a9c94a16fe2e127d5e8e830e80cb9d7
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-11-24 11:24:14 -0300

    simplification

--- haskell/anagram/Anagram.hs
+++ haskell/anagram/Anagram.hs
@@ -5,8 +5,7 @@ import Data.List(sort)
 import Data.Char(toLower)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor orig = foldr append []
-  where append w matches = if isAnagram orig w then w : matches else matches
+anagramsFor orig = filter (isAnagram orig)
 
 isAnagram :: String -> String -> Bool
 isAnagram a b = notSame && anagram
