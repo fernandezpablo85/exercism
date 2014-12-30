5e499a55a0e5767dd5a9e12d4304bd96cd88ff94
Pablo Fernandez <fernandezpablo85@gmail.com>
2014-12-30 15:16:09 -0300

    so much for prime factoring

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -3,55 +3,23 @@ package raindrops
 import "fmt"
 
 func Convert(n int) string {
-	pling := ""
-	plang := ""
-	plong := ""
-	factors := primeFactors(n)
+	conversion := ""
 
-	if contains(3, factors) {
-		pling = "Pling"
+	if n%3 == 0 {
+		conversion += "Pling"
 	}
 
-	if contains(5, factors) {
-		plang = "Plang"
+	if n%5 == 0 {
+		conversion += "Plang"
 	}
 
-	if contains(7, factors) {
-		plong = "Plong"
+	if n%7 == 0 {
+		conversion += "Plong"
 	}
 
-	anyFactor := len(pling) > 0 || len(plang) > 0 || len(plong) > 0
-
-	if anyFactor {
-		return fmt.Sprintf("%s%s%s", pling, plang, plong)
+	if len(conversion) > 0 {
+		return fmt.Sprintf("%s", conversion)
 	} else {
 		return fmt.Sprintf("%d", n)
 	}
-}
-
-func contains(n int, ns []int) bool {
-	for _, i := range ns {
-		if i == n {
-			return true
-		}
-	}
-	return false
-}
-
-func primeFactors(n int) []int {
-	var factors []int
-	for i := 2; i <= n; i++ {
-		if n%i == 0 && IsPrime(i) {
-			factors = append(factors, i)
-		}
-	}
-	return factors
-}
-
-func IsPrime(n int) bool {
-	prime := true
-	for i := (n - 1); i > 1 && prime; i-- {
-		prime = prime && (n%i != 0)
-	}
-	return prime
 }
--- go/raindrops/raindrops_test.go
+++ go/raindrops/raindrops_test.go
@@ -40,16 +40,3 @@ func BenchmarkConvert(b *testing.B) {
 		}
 	}
 }
-
-func TestPrime(t *testing.T) {
-	assert(17, IsPrime(17), t)
-	assert(2, IsPrime(2), t)
-	assert(3, IsPrime(3), t)
-	assert(8, !IsPrime(8), t)
-}
-
-func assert(n int, statement bool, t *testing.T) {
-	if !statement {
-		t.Errorf("assertion failed for %d", n)
-	}
-}
