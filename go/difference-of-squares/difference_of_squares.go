1c080d375785bdd99541472905728aa5c003da6b
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-01-01 13:27:19 -0300

    add memoization

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -4,7 +4,7 @@ import "math"
 
 func SquareOfSums(n int) int {
   sum := 0
-  for i := 0; i <= n; i++ {
+  for i := 1; i <= n; i++ {
     sum += i
   }
   return pow2(sum)
@@ -12,7 +12,7 @@ func SquareOfSums(n int) int {
 
 func SumOfSquares(n int) int {
   sum := 0
-  for i := 0; i <= n; i++ {
+  for i := 1; i <= n; i++ {
     sum += pow2(i)
   }
   return sum
@@ -22,6 +22,17 @@ func Difference(n int) int {
   return SquareOfSums(n) - SumOfSquares(n)
 }
 
+var mem = make(map[int]int)
 func pow2(n int) int {
+  if mem[n] > 0 {
+    return mem[n]
+  } else {
+    pow := doPow(n)
+    mem[n] = pow
+    return pow
+  }
+}
+
+func doPow(n int) int {
   return int(math.Pow(float64(n), float64(2)))
 }
