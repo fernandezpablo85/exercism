81d70f03470f188286b98711a1f5eb2f70ab5db1
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-01-04 15:56:00 -0300

    simplify algorithm

--- haskell/phone-number/Phone.hs
+++ haskell/phone-number/Phone.hs
@@ -4,32 +4,19 @@ module Phone(areaCode, number, prettyPrint) where
 
 import Data.Char(isDigit)
 
-clean :: String -> String
-clean = stripExtra1 . filter isDigit
-
-stripExtra1 :: String -> String
-stripExtra1 n = if extra1 then drop 1 n else n
-  where extra1 = length n == 11 && head n == '1'
-
--- ideally these woulde be ifValid (take 3) and ifValid id
--- can't figure out how to do it (yet)
-areaCode :: String -> String
-areaCode n = ifValid (take 3) (clean n)
-
 number :: String -> String
-number n = ifValid id (clean n)
-
-ifValid :: (String -> String) -> String -> String
-ifValid f n
-  | valid n = f n
+number n
+  | length digits == 10 = digits
+  | length digits == 11 && head digits == '1' = tail digits
   | otherwise = "0000000000"
+ where digits = filter isDigit n
 
-valid :: String -> Bool
-valid = (==10) . length
+areaCode :: String -> String
+areaCode = take 3 . number
 
 prettyPrint :: String -> String
 prettyPrint n = area ++ first ++ "-" ++ second
   where
     area = "(" ++ areaCode n ++ ") "
-    first = drop 3 . take 6 $ clean n
-    second = drop 6 $ clean n
+    first = drop 3 . take 6 $ number n
+    second = drop 6 $ number n
