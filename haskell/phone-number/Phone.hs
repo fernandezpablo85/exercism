e88b67d5a320f269863acafe8e81a6099fa4f2d9
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-01-05 18:40:53 -0300

    make fewer calls to number

--- haskell/phone-number/Phone.hs
+++ haskell/phone-number/Phone.hs
@@ -1,22 +1,25 @@
 {-# OPTIONS_GHC -Wall #-}
-
 module Phone(areaCode, number, prettyPrint) where
-
 import Data.Char(isDigit)
 
 number :: String -> String
 number n
-  | length digits == 10 = digits
-  | length digits == 11 && head digits == '1' = tail digits
+  | size == 10 = digits
+  | size == 11 && head digits == '1' = tail digits
   | otherwise = "0000000000"
- where digits = filter isDigit n
+  where
+    digits = filter isDigit n
+    size = length digits
 
 areaCode :: String -> String
-areaCode = take 3 . number
+areaCode ns = area
+  where (area, _, _) = pieces ns
+
+pieces :: String -> (String, String, String)
+pieces ns = (area, first, second)
+  where (area, rest) = splitAt 3 $ number ns
+        (first, second) = splitAt 3 rest
 
 prettyPrint :: String -> String
-prettyPrint n = area ++ first ++ "-" ++ second
-  where
-    area = "(" ++ areaCode n ++ ") "
-    first = drop 3 . take 6 $ number n
-    second = drop 6 $ number n
+prettyPrint n = "(" ++ area ++ ") " ++ first ++ "-" ++ second
+  where (area, first, second) = pieces n
