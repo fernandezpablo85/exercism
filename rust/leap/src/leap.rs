2beb4a8fe271fc738c923fa0d12939f5c9580338
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-07-05 01:20:48 -0300

    final leap.rs version

--- rust/leap/src/leap.rs
+++
@@ -1,5 +0,0 @@
-pub mod leap {
-  pub fn is_leap_year(y: u8) -> bool {
-    y > 10
-  }
-}
--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,1 +1,15 @@
-pub mod leap;
+/// tests if a year is a leap year.
+///
+/// # Examples
+///
+/// ```
+/// use leap;
+///
+/// assert_eq!(leap::is_leap_year(1990), false);
+/// assert_eq!(leap::is_leap_year(1984), true);
+/// ```
+pub fn is_leap_year (y: i32) -> bool {
+  let divisible_by = |n: i32| -> bool { y % n == 0 };
+  (divisible_by(400)) || (divisible_by(4) && !divisible_by(10))
+}
+
